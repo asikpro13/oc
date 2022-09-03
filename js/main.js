e879eda7b16be37e2d8c08bc9067c5a5ddc56dc6
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-09-03 20:47:14 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+//click
+document.body.onload = () => {
+    document.querySelector("#btnClick").dispatchEvent(new Event("click"));
+}
+
+//burger
 const menu = document.getElementById("menu");
 const burger = document.querySelector(".burger");
 
@@ -7,6 +13,7 @@ burger.addEventListener("click", () => {
     menu.classList.toggle("flex");
 })
 
+// Dark mode
 const modeDark = document.getElementById("modeDark");
 const modeWhite = document.getElementById("modeWhite");
 const htmlTag = document.querySelector("html");
@@ -24,7 +31,6 @@ function togleTheme(tagOne, tagTwo, htmlTag) {
     })
 }
 
-// Dark mode
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
 } else {
@@ -34,53 +40,18 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 togleTheme(modeDark, modeWhite, htmlTag)
 togleTheme(modeWhite, modeDark, htmlTag)
 
-// create an array of objects with the id, trigger element (eg. button), and the content element
-const accordionItems = [
-    {
-        id: 'accordion-example-heading-1',
-        triggerEl: document.querySelector('#accordion-example-heading-1'),
-        targetEl: document.querySelector('#accordion-example-body-1'),
-        active: true
-    },
-    {
-        id: 'accordion-example-heading-2',
-        triggerEl: document.querySelector('#accordion-example-heading-2'),
-        targetEl: document.querySelector('#accordion-example-body-2'),
-        active: false
-    },
-    {
-        id: 'accordion-example-heading-3',
-        triggerEl: document.querySelector('#accordion-example-heading-3'),
-        targetEl: document.querySelector('#accordion-example-body-3'),
-        active: false
-    }
-];
+//scroll
+const anchors = document.querySelectorAll('a[href*="#"]')
 
-// options with default values
-const options = {
-    alwaysOpen: true,
-    activeClasses: 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white',
-    inactiveClasses: 'text-gray-500 dark:text-gray-400',
-    onOpen: (item) => {
-        console.log('accordion item has been shown');
-        console.log(item);
-    },
-    onClose: (item) => {
-        console.log('accordion item has been hidden');
-        console.log(item);
-    },
-    onToggle: (item) => {
-        console.log('accordion item has been toggled');
-        console.log(item);
-    },
-};
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
 
-const accordion = new Accordion(accordionItems, options);
-// open accordion item based on id
-accordion.open('accordion-example-heading-2');
+        const blockID = anchor.getAttribute('href').substr(1)
 
-// close accordion item based on id
-accordion.close('accordion-example-heading-2');
-
-// toggle visibility of item based on id
-accordion.toggle('accordion-example-heading-3');
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
